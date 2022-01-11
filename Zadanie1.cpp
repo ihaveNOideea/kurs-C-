92397971cf0d37332d0c8219582822133e318a95
ihaveNOidea <97526518+ihaveNOideea@users.noreply.github.com>
2022-01-11 14:58:37 +0100

    Create Zadanie1.cpp

---
+++ Zadanie1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+  string imie="Kornel", nazw=" Piotrowicz", pełne;
+  int wiek=21;
+  pełne=imie + nazw;
+
+   cout<<"Witaj "<< pełne<< endl;
+   cout<<"Masz: "<< wiek;
+
+ return 0;
+}
